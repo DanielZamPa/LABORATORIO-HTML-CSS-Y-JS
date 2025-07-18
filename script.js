81c1e15a301a33d2a8f99d6d883f58e7614ceb71
Daniel Zamora Paez <[email>
2025-07-18 16:20:14 -0500

    Estructura HTML

---
+++ script.js
@@ -0,0 +1,18 @@
+let num1 = 0;
+let num2 = 0; 
+document.getElementById("Aumentar").addEventListener("click", function(){
+    if (texto){
+        const span = document.
+        li.textContent = texto;
+        document.getElementById("lista-tareas").appendChild(li)
+        input.value = ""
+    }
+})
+document.getElementById("Disminuir").addEventListener("click", function(){    
+    if (texto){
+        
+        li.textContent = texto;
+        document.getElementById("lista-tareas").appendChild(li)
+        input.value = ""
+    }
+})
