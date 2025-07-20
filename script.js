3ebb8b9ef2a80b758a7d4f991ccab6b58bbeff79
Daniel Zamora Paez <[email>
2025-07-20 16:25:19 -0500

    Funcionalidad del Contador

--- script.js
+++ script.js
@@ -1,20 +1,19 @@
-let num1 = 0;
-let num2 = 0;
+let num = 0;
 const spanNum = document.getElementById('num');
 function actualizar(){
-    if (num2>9){
+    document.getElementById('num').textContent = num
+    if (num>9){
         spanNum.style.color = '#44BBA4';        
     }
     else{
-        document.getElementById('num2').textContent = num2
         spanNum.style.color = '#000000';
     }
 }
 document.getElementById("Aumentar").addEventListener("click", function(){
-    num2 += 1;
+    num += 1;
     actualizar();
 })
 document.getElementById("Disminuir").addEventListener("click", function(){    
-        num2 -= 1;
+        num -= 1;
         actualizar();
 })
